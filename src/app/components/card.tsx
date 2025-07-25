d6f0e1dd7afaa65f31d6e38071a39b098f6a5d8a
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2025-07-25 14:01:24 +0530

    improvements

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -16,7 +16,7 @@ type CardProps = {
 export default function Card(props:CardProps) {
   
     return (
-        <div className="max-w-md w-[320px] md:h-xl  min-h-sm max-h-5xl bg-slate-800 rounded-sm p-7 flex flex-col gap-y-5">
+        <div className="max-w-md w-[350px] md:h-xl  min-h-sm max-h-5xl bg-slate-800 rounded-sm px-7 py-10 flex flex-col gap-y-5">
               <div className="flex justify-between items-center ">
                 <CodeXml className="text-emerald-600 font-extrabold size-10"/>
                 <div className="flex items-center gap-x-5">
@@ -35,7 +35,7 @@ export default function Card(props:CardProps) {
                 <div className="text-sm bg-slate-600 p-2 rounded-full">reactjs</div>
                 <div className="text-sm bg-slate-600 p-2 rounded-full">reactjs</div> */}
               {props.value.techStack.map((i, key) => (
-                <div key={key} className="text-sm bg-slate-600 p-2 rounded-full">{i}</div>
+                <div key={key} className="px-4 py-2 bg-emerald-500/20 text-emerald-300 rounded-full text-sm border border-emerald-500/30 hover:bg-emerald-500/30 hover:scale-110 transition-all duration-300">{i}</div>
               ))}
               </div>
             </div>
